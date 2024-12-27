f902fda83f0865ae3290686cfe4c539f9196d0c6
Pedro Almada <palmada@gmail.com>
2024-12-27 14:02:15 +0000

    index.html: add decorative currency symbol
    Adds a currency symbol that the user can type in for the money inputs.
    
    The symbols are spans placed after each input form with the class "currency-symbol" that a little js function updates when a new currency symbol is typed.
    
    This is purely decorative, however.

---
+++ js/update_currency.js
@@ -0,0 +1,22 @@
+/* © Copyright 2024, Pedro Almada
+Licensed under the GPL-3.0-or-later
+
+This file is part of the House buying calculator and is distributed WITHOUT ANY WARRANTY;
+without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with the house buying calculator software.
+If not, see <https://www.gnu.org/licenses/>.
+*/
+
+/**
+ * Updates the currency symbols in the inputs section.
+ */
+function updateCurrency() {
+    let currency = document.getElementById("currency").value;
+    const currencyElements = document.querySelectorAll('.currency-symbol');
+
+    currencyElements.forEach(element => {
+        element.innerHTML = currency
+    });
+}

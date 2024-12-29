2a11fa8cf2550aca5b7190fb332b9f1a46b73716
Pedro Almada <palmada@gmail.com>
2024-12-29 16:15:53 +0000

    feat: add birthdate validation.
    The birthday was allowed to be set in the future.
    
    This adds a maximum to the date widget and if a user types an incorrect value, resets to a default 30 years of age as people younger than that have better things to think about.

---
+++ js/validate.js
@@ -0,0 +1,28 @@
+/* © Copyright 2024, Pedro Almada
+Licensed under the GPL-3.0-or-later
+
+This file is part of the House buying calculator and is distributed WITHOUT ANY WARRANTY;
+without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with the house buying calculator software.
+If not, see <https://www.gnu.org/licenses/>.
+*/
+
+
+/**
+ * Validates the input boxes, setting a default value if they are not correct.
+ *
+ * @param id The ID of the input to validate as this only validates one element at a time.
+ * @param defaultValue The default value to set if validation fails.
+ */
+function validate(id, defaultValue = 0) {
+    let element = document.getElementById(id);
+    switch(id) {
+        case 'birth_date':
+            if (DateTime.fromISO(element.value) >= DateTime.now()) {
+                element.value = defaultValue;
+            }
+            break;
+    }
+}

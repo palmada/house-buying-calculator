9c14e9c1f6c2107ba5674f8b978bf4be91405399
Pedro Almada <palmada@gmail.com>
2024-12-29 15:35:50 +0000

    feat: add ability to export inputs as URL parameters
    We had a way to read in inputs as URL parameters but no way to export them so URL's could be shared.
    
    This adds an export button at the end of the page to do so.

--- js/parameters.js
+++ js/parameters.js
@@ -39,3 +39,29 @@ function readSelection(id, url_params) {
 
     document.getElementById(id).value = value;
 }
+
+/**
+ * Exports the input values provided by the user as URL parameters.
+ *
+ * @returns A URL string with the input values stored as URL parameters.
+ */
+function exportAsURL() {
+    let parameters = new URLSearchParams()
+
+    let appendParameter = (id) =>  parameters.append(id, document.getElementById(id).value)
+
+    appendParameter('house_price');
+    appendParameter('savings');
+    appendParameter('monthly_savings');
+    appendParameter('interest_rate');
+    appendParameter('min_deposit');
+    appendParameter('rent');
+    appendParameter('birth_date');
+    appendParameter('retirement_age');
+    appendParameter('max_mortgage_length');
+    appendParameter('taxes');
+    appendParameter('custom_tax_amount');
+
+    return window.location.href.split("?")[0] + "?" + parameters.toString()
+}
+

7380b5f84a8619204235ca72a72d52fbe1bf389e
Pedro Almada <palmada@gmail.com>
2025-01-04 17:37:15 +0000

    feat: make currency selector a dropdown
    Typing in your desired symbol was annoying, I prefer having a hard-coded list.
    
    ChatGPT helped me with a function to extract it from UTF-8.

--- js/update_currency.js
+++ js/update_currency.js
@@ -1,4 +1,4 @@
-/* © Copyright 2024, Pedro Almada
+/* © Copyright 2024-2025, Pedro Almada
 Licensed under the GPL-3.0-or-later
 
 This file is part of the House buying calculator and is distributed WITHOUT ANY WARRANTY;
@@ -8,6 +8,72 @@ See the GNU General Public License for more details.
 You should have received a copy of the GNU General Public License along with the house buying calculator software.
 If not, see <https://www.gnu.org/licenses/>.
 */
+
+
+/**
+ * List of currency symbols extracted from UTF-8
+ *
+ * @type {string[]}
+ */
+const CURRENCIES = [
+    '€',
+    '£',
+    '$',
+    '¢',
+    '¤',
+    '¥',
+    '֏',
+    '؋',
+    '߾',
+    '߿',
+    '৲',
+    '৳',
+    '৻',
+    '૱',
+    '௹',
+    '฿',
+    '៛',
+    '₠',
+    '₡',
+    '₢',
+    '₣',
+    '₤',
+    '₥',
+    '₦',
+    '₧',
+    '₨',
+    '₩',
+    '₪',
+    '₫',
+    '₭',
+    '₮',
+    '₯',
+    '₰',
+    '₱',
+    '₲',
+    '₳',
+    '₴',
+    '₵',
+    '₶',
+    '₷',
+    '₸',
+    '₹',
+    '₺',
+    '₻',
+    '₼',
+    '₽',
+    '₾',
+    '₿',
+    '⃀',
+    '꠸',
+    '﷼',
+    '﹩',
+    '＄',
+    '￠',
+    '￡',
+    '￥',
+    '￦'
+];
 
 /**
  * Updates the currency symbols in the inputs section.
